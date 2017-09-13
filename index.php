8d2e56863b98ff88bfa0357d460a0dd8d68703a7
Vio Nikolov <vi0nikk@gmail.com>
2017-09-14 00:33:15 +0300

    sign up - fron end

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Vio
- * Date: 13-Sep-17
- * Time: 22:55:41
- */
+<!doctype html>
+    <html>
+        <head>
+            <title>
+                Simple login form
+            </title>
+            <link href="./css/style.css" rel="stylesheet">
+        </head>
+        <body>
+            <div class="main">
+                <div class="container">
+                    <h2>Sign up</h2>
+                    <form method="get" action="">
+                        <ul>
+                            <li>
+                                <label for="fname">First name</label>
+                            </li>
+                            <li>
+                                <input id="fname" type="text" name="fname">
+                            </li>
+                            <li>
+                                <label for="lname">Last name</label>
+                            </li>
+                            <li>
+                                <input id="lname" type="text" name="lname">
+                            </li>
+                            <li>
+                                <label for="email">Email</label>
+                            </li>
+                            <li>
+                                <input id="email" type="email" name="email">
+                            </li>
+                            <li>
+                                <label for="pass">Password (6 or more characters)</label>
+                            </li>
+                            <li>
+                                <input id="pass" type="password" name="pass">
+                            </li>
+                        </ul>
+
+                        <p>By clicking Sign Up, you agree to <span>User Agreemen</span>, <span>Privacy Policy</span>, and <span>Cookie Policy</span></p><br>
+                        <button id="signup" type="submit"><b>Sign Up</b></button>
+                    </form>
+                </div>
+            </div>
+        </body>
+    </html>
